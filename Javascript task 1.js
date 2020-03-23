9bef9151ec25456e4b330b199f048b1b695bcfd8
francoiscooljazz <61973366+francoiscooljazz@users.noreply.github.com>
2020-03-23 21:29:48 +0200

    Add files via upload

---
+++ Javascript task 1.js
@@ -0,0 +1,15 @@
+let fullname = "Francois" + " " + "Williams";
+console.log("My name is:" + fullname);
+
+let myCourses = ["Design", "Python", "JavaScript", "CSS", "HTML"];
+console.log(myCourses);
+
+
+const myCourses = [];
+for (let i = 1; i < 200; i+=2) {
+ {
+myCourses.push(i);
+ }
+}
+    console.log(myCourses);
+    
